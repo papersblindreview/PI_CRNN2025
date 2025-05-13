6ea1945c706178d48311b1276c1cc8663ef340e8
papersblindreview <lmenical@nd.edu>
2025-05-13 18:59:26 +0200

    Update uq.py

--- code/uq.py
+++ code/uq.py
@@ -65,9 +65,12 @@ sequence_generator.load_weights('sequence_generator.weights.h5')
 # LOADING DATA
 const_dict = load_constants_long()
 Uf, P, T_h, T_0, Pr, Ra = get_model_constants(const_dict)
-data_train, data_val, x, z, t = load_data(2000, 500, Uf, P, T_h, T_0, offset=2)
+train_size, val_size = 2000, 500
+data_train, data_val, x, z, t = load_data(train_size, val_size, Uf, P, T_h, T_0, offset=2)
 uwpT = np.concatenate((data_train, data_val), axis=0, dtype=np.float32)
 
+look_back = 80
+ensembles = 50
 horizon = 108 # 2 turnover times
 
 
@@ -79,3 +82,54 @@ def get_forecast(input_rbc, horizon=horizon):
   x = sequence_generator(input_encoder[:,-1:], h, c, horizon, training=False)
   forecast = ae_decoder(x, training=False) 
   return np.asarray(forecast[0])
+
+# the calibration forecasts begin AFTER the training window
+calibration_starts = np.linspace(train_size, train_size+val_size-horizon-1, ensembles).astype('int')
+
+residuals = []
+for s in calibration_starts:
+  forecast_temp = get_forecast(uwpT[(s-look_back):s])
+  residuals.append(forecast_temp - uwpT[s:(s+horizon)])
+
+residuals = np.asarray(residuals)
+
+confidences = [0.8, 0.90, 0.95]
+adj = np.array([0.97, 1.01, 0.85, 1.04]).reshape((1,1,1,4)) # found via cross validation
+
+residuals_abs = np.abs(residuals)
+     
+for c in confidences:
+  q = np.quantile(residuals_abs, c, axis=0)
+  
+  coverage = []
+  for i, s in enumerate(calibration_starts):
+    forecast_temp = get_forecast(uwpT[(s-look_back):s])
+    temp_lo, temp_up = forecast_temp - adj*q, forecast_temp + adj*q
+    cov = ( (uwpT[s:(s+horizon)] > temp_lo) & (uwpT[s:(s+horizon)] < temp_up) ).mean(axis=(0,1,2))
+    coverage.append(cov)
+  
+  coverage = np.asarray(coverage)
+  coverage_m = np.median(coverage, axis=0)
+  coverage_q1 = np.quantile(coverage, 0.25, axis=0)
+  coverage_q2 = np.quantile(coverage, 0.75, axis=0)
+  iqr = coverage_q2 - coverage_q1
+  print(f'\nCoverages ({int(100*c)}%):')
+  for i, v in enumerate(['u','w','p','T']):
+    print(f'{v}. {coverage_m[i]:.4f} ({iqr[i]:.4f})') 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
